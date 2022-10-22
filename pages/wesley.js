82c96a8b120d29c97ac531a8498674db7e1bf47a
alyssam723 <71541413+alyssam723@users.noreply.github.com>
2022-10-21 22:18:28 -0400

    added pictures of us

--- pages/wesley.js
+++ pages/wesley.js
@@ -5,10 +5,10 @@ import styles from '../styles/Wesley.module.css'
 import Image from 'next/image';
 import wesley from '../images/wesley.png';
 
-export default function Contact(){
+export default function Wesley(){
     return(
         <div className={styles.container}>
-            <h2 className={styles.title}>Contact Page</h2>
+            <h2 className={styles.title}>Wesley Page</h2>
             <div className={styles.details}>
             <Image className= {styles.image} src={wesley} />
             </div>
